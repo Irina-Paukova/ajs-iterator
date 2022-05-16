6960f94a539f04657e10415bd46f744054aae8db
Irina Paukova <63339174+Irina-Paukova@users.noreply.github.com>
2022-05-16 16:49:53 +0300

    1st commit

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,19 @@
+import Person from '../person';
+import Team from '../team';
+
+test('...', () => {
+    const char1 = new Person("Nikita", "Bowman");
+    const char2 = new Person("Nikita", "Daemon");
+    const char3 = new Person("Nikita", "Magician");
+    const expected = new Team([char1, char2, char3]);
+    const testTeam = [];
+
+    for(let member of expected) {
+        testTeam.push(member);
+    }
+
+    expect(expected.innerTeam).toEqual(testTeam);
+});
+
+
+// for (letnum of range) {console.log(num)};
---
+++ src/js/team.js
@@ -0,0 +1,25 @@
+export default class Team {
+    constructor(team) {
+        this.innerTeam = team;
+    }
+
+    [Symbol.iterator] () { 
+        let lengthArr = 0;
+
+        return {
+            next: () => {
+                if (this.innerTeam.length > lengthArr) {
+                    lengthArr++;
+
+                    return {
+                         done: false,
+                         value: this.innerTeam[lengthArr - 1]
+                    };
+                } 
+                return {
+                    done: true
+                };
+            }
+        }
+    };
+}
